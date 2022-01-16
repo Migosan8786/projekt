cbc3654db18eec2410f6e9c390cf7fff34b4d2ce
Migosan8786 <amirhauck5501868@mail.ru>
2022-01-16 16:48:52 +0200

    first commit

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 
 #BOT:
-TOKEN = "" # ТОКЕН БОТА
-ADMIN = 392192947 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+TOKEN = "5032632076:AAF9baTI2FHSB6Z0AZ7ZUZS8osyuXfJ1yhQ" # ТОКЕН БОТА
+APP_URL = "https://mangao.herokuapp.com/" + TOKEN
+ADMIN = 1500022449 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
 
 DIR = "" # Путь к файлу, если нужно
 
 #USER: # https://my.telegram.org/auth
-API_ID = 3232323
-API_HASH = ""
+API_ID = 12857060
+API_HASH = "e8f427486a0d051895aa38b6b5cc1597"
 
-# При поддержке каналана https://t.me/slivmenss
+#
