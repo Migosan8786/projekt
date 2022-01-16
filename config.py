0e8ad087c0339656b331cc52faf8e09e890db92d
Migosan8786 <amirhauck5501868@mail.ru>
2022-01-16 14:59:19 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+
+#BOT:
+TOKEN = "" # ТОКЕН БОТА
+ADMIN = 392192947 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 3232323
+API_HASH = ""
+
+# При поддержке каналана https://t.me/slivmenss
